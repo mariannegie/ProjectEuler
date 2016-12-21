bcc3b60efa5f524bc16feb50b61658c9f6f9367e
maria_000 <maria_000@Brad.home>
2016-12-20 20:39:33 -0600

    More efficient algorithm to find primes

--- ProjectEuler/src/euler/Problem3.java
+++ ProjectEuler/src/euler/Problem3.java
@@ -8,17 +8,17 @@ public class Problem3
     public static void main(String[] args)
     {
         long num = 600851475143L;
-        List<Long> factors = getPrimeFactors(num);
+        List<Integer> factors = getPrimeFactors(num);
         System.out.println(factors.get(factors.size()-1));
     }
 
     // I know these methods are going to be reused,
     // so make them easy to factor out later
-    private static List<Long> getPrimeFactors(long number)
+    private static List<Integer> getPrimeFactors(long number)
     {
-        List<Long> primes = getPrimes((long)Math.floor(Math.sqrt(number)));
-        List<Long> factors = new ArrayList<>();
-        for(long p: primes)
+        List<Integer> primes = getPrimes((int)Math.floor(Math.sqrt(number)));
+        List<Integer> factors = new ArrayList<>();
+        for(int p: primes)
         {
             if(number % p == 0)
             {
@@ -28,23 +28,27 @@ public class Problem3
         return factors;
     }
 
-    private static List<Long> getPrimes(long max)
+    private static List<Integer> getPrimes(int max)
     {
-        List<Long> primes = new ArrayList<>();
-        for(long i=2; i<=max; i++)
+        boolean[] primeArray = new boolean[max+1];
+        // Call 0 and 1 not prime off the bat
+        for(int i=2; i<max; i++)
         {
-            boolean factorFound = false;
-            for(long p: primes)
-            {
-                if(i % p == 0)
-                {
-                    factorFound = true;
-                    break;
-                }
-            }
-            if(!factorFound)
+            primeArray[i] = true;
+        }
+
+        List<Integer> primes = new ArrayList<>();
+        for(int i=2; i<=max; i++)
+        {
+            if(primeArray[i])
             {
                 primes.add(i);
+                int j = i + i;
+                while(j<max)
+                {
+                    primeArray[j] = false;
+                    j += i;
+                }
             }
         }
         return primes;
